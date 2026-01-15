41fae7a9a79c671f72671d96f175790d70156eda
Naveengupta2006 <naveengupta931844@gmail.com>
2026-01-15 14:30:31 +0530

    Subquery Question

--- SQL_logic_Question/subquery_2.sql
+++ SQL_logic_Question/subquery_2.sql
@@ -23,3 +23,37 @@ from employees as e
 where salary = (select min(salary)
 from employees
 where department = e.department)
+
+-- find department where average salary is greater than overall average salary.
+select department 
+from employees
+group by department 
+having  avg(salary) > (select avg(salary)
+from employees)
+
+-- find employees who are not earning the maximum salary in their department.
+select *
+from employees as e
+where salary < (select max(salary)
+from employees
+where department = e.department)
+
+-- find the department with the highest average salary.
+select department
+from employees
+group by department
+having max(salary) > (select avg(salary)
+from employees)
+
+-- find the department with the maximum number of employees.
+select department
+from employees
+group by department
+having count(*) = (select max(emp_count)
+from (
+    select count(*) as emp_count
+    from employees
+    group by department
+) t
+)
+
