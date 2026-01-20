e76b33366d610bfaf05acbd10241e28cb7447b75
Naveengupta2006 <naveengupta931844@gmail.com>
2026-01-20 22:38:07 +0530

    Row_number() Logic Question

---
+++ SQL_logic_Question/windows.sql
@@ -0,0 +1,20 @@
+select * from employees
+
+-- Fetch the top 2 employees from each department using ROW_NUMBER().
+select *
+from(
+    select *,
+    row_number() over(PARTITION BY dept_id order by salary desc) as top_two
+    from employees
+) t 
+where top_two <= 2
+
+-- Find employees whose row number is between 2 and 4 based on salary.
+select *
+from (
+    select *,
+    row_number() over(order by salary desc) as salary_bet
+    from employees
+
+) t 
+where salary_bet BETWEEN 2 and 4;
