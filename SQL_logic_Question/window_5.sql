990574dd5a90bd81b6a57d5c2f8b52f88786995a
Naveengupta2006 <naveengupta931844@gmail.com>
2026-01-22 23:29:54 +0530

    scenario based question

---
+++ SQL_logic_Question/window_5.sql
@@ -0,0 +1,19 @@
+select *from employee
+
+-- fetch the highest paid employee per department.
+select *
+from(
+    select *,
+    DENSE_RANK() over(PARTITION BY dept_id order by salary desc) as de_salary
+    from employee
+) t
+where de_salary = 1
+
+-- fetch all employee sharing the highest salary.
+select *
+from(
+    select *,
+    dense_rank() over(order by salary desc) as rak
+    from employee 
+) t
+where rak = 1
